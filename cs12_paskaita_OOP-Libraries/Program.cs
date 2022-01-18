a78698021d95a16157ebf2464d4ba24b23c854d2
Surkus <v.surkus@me.com>
2022-01-18 20:14:50 +0200

    Initial commit

---
+++ cs12_paskaita_OOP-Libraries/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace cs12_paskaita_OOP_Libraries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
